03251c1f9ef8727998e22cefc94869ec924a6137
Tejas Pratap <sms2cr7@gmail.com>
2026-04-24 08:02:29 +0530

    deployment changes

--- application.py
+++ application.py
@@ -1,1 +1,10 @@
-from app import app as application
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route('/')
+def home():
+    return "APP WORKING SUCCESSFULLY"
+
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", port=8000)
