0b001ab341196d11962c572bef59105028de6f9f
Tejas Pratap <sms2cr7@gmail.com>
2026-04-24 08:35:32 +0530

    deployment changes2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("/n"," ") for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
 
         if Hypen_E_Dot in requirements:
             requirements.remove(Hypen_E_Dot)
